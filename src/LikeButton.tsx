621f5c2eea452c7731923cf4b1d4918039f99084
Vladislav Kishkovskiy <67899905+aDeepRecession@users.noreply.github.com>
2024-11-10 13:49:48 +0300

    style(likeBtn): change color to light red

--- src/LikeButton.tsx
+++ src/LikeButton.tsx
@@ -35,8 +35,8 @@ const LikeSvg = ({ isLiked = false }: LikeSvgProps) => {
       <g id="SVGRepo_iconCarrier">
         <path
           d="M237.376,436.245l0.774,0.976c210.94-85.154,292.221-282.553,199.331-367.706 c-92.899-85.154-199.331,30.953-199.331,30.953h-0.774c0,0-106.44-116.107-199.331-30.953 C-54.844,154.658,26.437,351.092,237.376,436.245z"
-          fill={isLiked ? "red" : "white"}
-          stroke="black"
+          fill={isLiked ? "#e57373" : "white"}
+          stroke={isLiked ? "white" : "black"}
           strokeWidth="20"
         />
       </g>
