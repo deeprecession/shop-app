c428daf8db62a381a1fb4a7d58a0edcbdd000838
Vladislav Kishkovskiy <67899905+aDeepRecession@users.noreply.github.com>
2024-11-13 21:04:28 +0300

    fix(ProductListPage): fix link to individual product pages

--- src/pages/productList/ProductList.tsx
+++ src/pages/productList/ProductList.tsx
@@ -58,7 +58,7 @@ const ProductListElement: React.FC<ProductListElementProps> = ({
   };
 
   return (
-    <Link className="product-link" to={`products/${product.id.toString()}`}>
+    <Link className="product-link" to={`${product.id.toString()}`}>
       <article className="product-list-element">
         <div className="product-list-element__image">
           <img src={product.images[0]} alt={product.title}></img>
