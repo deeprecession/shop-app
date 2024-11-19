690c744eda1ec6c42288a550f99f3df30388234f
Vladislav Kishkovskiy <67899905+aDeepRecession@users.noreply.github.com>
2024-11-19 16:47:03 +0300

    feat(cart): add persistance store for shoppingCart

--- src/store.ts
+++ src/store.ts
@@ -7,20 +7,33 @@ import {
 import { setupListeners } from "@reduxjs/toolkit/query";
 import { catalogSlice } from "./features/catalog/catalogSlice";
 import { shoppingCartSlice } from "./features/shoppingCart/shoppingCartSlice";
+import storage from "redux-persist/lib/storage";
+import { PersistConfig, persistStore } from "redux-persist";
+import persistReducer from "redux-persist/es/persistReducer";
+
+const persistConfig: PersistConfig<any, any, any, any> = {
+	key: "root",
+	storage,
+	whitelist: ["shoppingCart"],
+};
 
 const rootReducer = combineSlices(catalogSlice, shoppingCartSlice);
+
+const persistentRootReducer = persistReducer(persistConfig, rootReducer);
 
 export type RootState = ReturnType<AppStore["getState"]>;
 
 export const makeStore = () => {
-	const store = configureStore({ reducer: rootReducer });
+	const store = configureStore({ reducer: persistentRootReducer });
 
 	setupListeners(store.dispatch);
 
-	return store;
+	const persistor = persistStore(store);
+
+	return [store, persistor];
 };
 
-export const store = makeStore();
+export const [store, persistor] = makeStore();
 
 export type AppStore = typeof store;
 
