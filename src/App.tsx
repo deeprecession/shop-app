dde9da323e25d95d573e7caa1d1a3a18bc8db0a0
Vladislav Kishkovskiy <67899905+aDeepRecession@users.noreply.github.com>
2024-11-20 21:12:28 +0300

    feat: add global layout div

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "./components/NavBar/NavBar";
+import "./styles/layout.css";
 
 const App = () => {
   return (
-    <div>
+    <div className="layout">
       <NavBar />
       <main>
         <Outlet />
       </main>
+
     </div>
   );
 };
