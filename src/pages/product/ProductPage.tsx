6981b9ca011227718f594d6a14efafb4d9781d6d
Vladislav Kishkovskiy <67899905+aDeepRecession@users.noreply.github.com>
2024-11-20 22:24:06 +0300

    refactor: remove back button and use more general button

--- src/pages/product/ProductPage.tsx
+++ src/pages/product/ProductPage.tsx
@@ -1,10 +1,13 @@
-import BackBtn from "../../components/BackBtn/BackBtn";
+import { useNavigate } from "react-router-dom";
+import Button from "../../components/BuyButton/Button";
 import { Product } from "./Product";
 
 const ProductPage = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="product-page">
-      <BackBtn />
+      <Button onClick={() => navigate(-1)} content="Back" />
       <Product />
     </div>
   );
