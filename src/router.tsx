c43bf2399980b0ea54818c6888a8c1eb39c8fbe9
Vladislav Kishkovskiy <67899905+aDeepRecession@users.noreply.github.com>
2024-11-23 16:46:30 +0300

    fix(router): add '#' prefix to base url

--- src/router.tsx
+++ src/router.tsx
@@ -6,31 +6,28 @@ import CatalogPage from "./pages/catalog/CatalogPage.tsx";
 import CartPage from "./pages/cart/CartPage.tsx";
 import App from "./App.tsx";
 
-export const router = createHashRouter(
-  [
-    {
-      path: "/",
-      element: <App />,
-      children: [
-        {
-          index: true,
-          element: <Home />,
-        },
-        {
-          path: "/catalog",
-          element: <CatalogPage />,
-        },
-        {
-          path: "/cart",
-          element: <CartPage />,
-        },
-        {
-          path: "/products/:productId",
-          loader: productLoader,
-          element: <ProductPage />,
-        },
-      ],
-    },
-  ],
-  { basename: import.meta.env.BASE_URL },
-);
+export const router = createHashRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "/catalog",
+        element: <CatalogPage />,
+      },
+      {
+        path: "/cart",
+        element: <CartPage />,
+      },
+      {
+        path: "/products/:productId",
+        loader: productLoader,
+        element: <ProductPage />,
+      },
+    ],
+  },
+]);
