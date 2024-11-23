37aa05832b482eaf04351a463315ef7ad9bfa763
Vladislav Kishkovskiy <67899905+aDeepRecession@users.noreply.github.com>
2024-11-23 16:16:43 +0300

    fix(router): add base url from vite to react-router

--- src/router.tsx
+++ src/router.tsx
@@ -6,28 +6,31 @@ import CatalogPage from "./pages/catalog/CatalogPage.tsx";
 import CartPage from "./pages/cart/CartPage.tsx";
 import App from "./App.tsx";
 
-export const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        index: true,
-        element: <Home />,
-      },
-      {
-        path: "/catalog",
-        element: <CatalogPage />,
-      },
-      {
-        path: "/cart",
-        element: <CartPage />,
-      },
-      {
-        path: "/products/:productId",
-        loader: productLoader,
-        element: <ProductPage />,
-      },
-    ],
-  },
-]);
+export const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <App />,
+      children: [
+        {
+          index: true,
+          element: <Home />,
+        },
+        {
+          path: "/catalog",
+          element: <CatalogPage />,
+        },
+        {
+          path: "/cart",
+          element: <CartPage />,
+        },
+        {
+          path: "/products/:productId",
+          loader: productLoader,
+          element: <ProductPage />,
+        },
+      ],
+    },
+  ],
+  { basename: import.meta.env.BASE_URL },
+);
